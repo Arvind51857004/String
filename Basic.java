e9a72883c689684d8968ec9a82b747e3940679b0
Arvind <arvindmd97@gmail.com>
2022-09-04 18:12:29 +0530

    string basics

---
+++ Basic.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String();
+		String s2=new String();
+
+	}
+
+}
